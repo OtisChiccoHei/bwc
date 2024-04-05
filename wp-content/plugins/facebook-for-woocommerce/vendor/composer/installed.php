fb9e6ac7aad27905cebe7540d37718e21ac3edf1
OtisChiccoHei <emyjrruivivar@gmail.com>
2024-04-05 13:29:24 +0800

    first commit

---
+++ wp-content/plugins/facebook-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'facebookincubator/facebook-for-woocommerce',
+        'pretty_version' => 'dev-release/3.1.13',
+        'version' => 'dev-release/3.1.13',
+        'reference' => 'e1d3aba56e1556d836e8fac862f43a5216bf49bf',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'facebookincubator/facebook-for-woocommerce' => array(
+            'pretty_version' => 'dev-release/3.1.13',
+            'version' => 'dev-release/3.1.13',
+            'reference' => 'e1d3aba56e1556d836e8fac862f43a5216bf49bf',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler-job-framework' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => 'b0b21b9cc87e476ba7f8817050b39274ea7d6732',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler-job-framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
